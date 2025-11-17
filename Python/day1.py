d5609501658e82bc8440999ef8b1ae1802733be5
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-17 18:18:28 +0545

    TypeCasting in Python

--- Python/day1.py
+++ Python/day1.py
@@ -1,10 +1,16 @@
 #data types
-integer = 7
-float = 7.12
-string = "Bipin"
-boolean = True
+integerVar = 7
+floatVar = 7.12
+stringVar = "7.12"
+booleanVar = True
 
-print(type(integer))
-print(type(float))
-print(type(string))
-print(type(boolean))
+print(type(integerVar))
+print(type(floatVar))
+print(type(stringVar))
+print(type(booleanVar))
+
+#typecasting
+typefloat = float(stringVar)
+print(typefloat, type(typefloat))
+typeInt = int(typefloat)
+print(typeInt, type(typeInt))
