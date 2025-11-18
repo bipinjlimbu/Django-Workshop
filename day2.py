cb9bc05f3fe57a77b8b1c23e2e60f23b2160962d
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-18 17:15:51 +0545

    Operators, Conditions and String in Python

---
+++ day2.py
@@ -0,0 +1,18 @@
+# # Operator and Condition
+# a1 = int(input("Enter Any Number:"))
+# a2 = int(input("Enter Any Number:"))
+# a3 = int(input("Enter Any Number:"))
+
+# if a1>a2 and a1>a3:
+#     print(f"{a1} is the Greatest Number")
+
+# elif a2>a1 and a2>a3:
+#     print(f"{a2} is the Greatest Number")
+
+# else:
+#     print(f"{a3} is the Greatest Number")
+
+#String
+fname = "Bipin"
+lname = "Limbu"
+print(f"Your Name is {fname} {lname}")
