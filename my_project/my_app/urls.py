af5240f89286c238069b29e5bd5273d556efac1f
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-04 11:48:12 +0545

    Project Urls Updated

--- my_project/my_app/urls.py
+++ my_project/my_app/urls.py
@@ -3,7 +3,7 @@ from .views.main_view import index_page
 from .views.auth_view import login_page, register_page
 
 urlpatterns = [
-    path('index/', index_page, name='index_page'),
+    path('', index_page, name='index_page'),
     path('login/', login_page, name='login_page'),
     path('register/',register_page, name='register_page')
 ]
