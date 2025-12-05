13223dbc25d4e5f98d7321dc7cb9063908cf84aa
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-05 12:46:39 +0545

    models in Django

--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class student(models.Model):
+    fullname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    fee = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)    
