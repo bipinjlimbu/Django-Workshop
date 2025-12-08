274ba15873753a997f2928e74b3d03c50c3eb0dc
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-08 12:42:24 +0545

    Create Operation in Django

--- my_project/my_app/views/main_view.py
+++ my_project/my_app/views/main_view.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from my_app.models import student
 
 def index_page(request):
     return render(request, 'main/index_page.html')
 
 def form_page(request):
+    if request.method == 'POST':
+        fullname = request.POST.get('fullname')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+        fee = request.POST.get('fee')
+        description = request.POST.get('description')
+        
+        student.objects.create(
+            fullname=fullname,
+            email=email,
+            address=address,
+            phone=phone,
+            fee=fee,
+            description=description
+        )
+        return redirect('index_page')
+    
     return render(request, 'main/student_form.html')
